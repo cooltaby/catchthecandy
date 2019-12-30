88d30726ea09209b5260fe1fca3cab0dea2e207d
cooltaby <cooltaby@users.noreply.github.com>
2019-12-30 11:24:08 +0530

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1577685282,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/circle-sheet0.png",
+		"images/circle-sheet1.png",
+		"images/clickrecord-sheet0.png",
+		"images/countdown-sheet0.png",
+		"images/countdown-sheet1.png",
+		"images/spawncirclebottomright-sheet0.png",
+		"images/hudbest-sheet0.png",
+		"images/hudscore-sheet0.png",
+		"images/hudtime-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/click.m4a",
+		"media/click.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
